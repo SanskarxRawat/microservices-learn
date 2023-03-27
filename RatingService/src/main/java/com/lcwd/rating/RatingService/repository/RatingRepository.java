abe01be444b8d3523005c6531a9f0dac0cae0818
Sanskar Rawat <rawatavi.211@gmail.com>
2023-03-27 10:58:31 +0530

    Add files via upload

---
+++ RatingService/src/main/java/com/lcwd/rating/RatingService/repository/RatingRepository.java
@@ -0,0 +1,15 @@
+package com.lcwd.rating.RatingService.repository;
+
+import com.lcwd.rating.RatingService.entities.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends MongoRepository<Rating,String> {
+
+
+    List<Rating> findByUserId(String userId);
+
+    List<Rating> findByHotelId(String hotelId);
+
+}
